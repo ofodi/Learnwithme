e8ee58d3db80708353ddc844ae1a5fb74ae685c5
Christabel Ofodi <ofodic@gmail.com>
2024-08-21 10:20:35 +0100

    Fix egress configuration for security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,44 +36,29 @@ resource "aws_iam_instance_profile" "ec2-profile" {
     role = "EC2-ECR-AUTH"
 }
 resource "aws_security_group" "maingroup" {
-    egress = [
-        {
-            cidr_blocks = ["0.0.0.0/0"]
-            description = ""
-            from_port = 0
-            ipv6_cidr_blocks = []
-            protocol = "-1"
-            security_groups = []
-            self = false
-            to_port = 0
-        }
-    ]
-    ingress =  [
-        {
-            cidr_blocks = ["0.0.0.0/0", ]
-            description = ""
-            from_port = 22
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []
-            protocol = "tcp"
-            security_groups = []
-            self = false
-            to_port = 22
-        },
-        {
-            cidr_blocks = ["0.0.0.0/0", ]
-            description = ""
-            from_port = 80
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []
-            protocol = "tcp"
-            security_groups = []
-            self = false
-            to_port = 80
-        }
-    ]
+  name        = "my-security-group"
+  description = "My security group"
 
-     
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+
+    ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
 }
 
 resource "aws_key_pair" "deployer" {
